680d1c73a00a2f0a7a74a9cc05096fb7ac11ca24
Johan <johan.aspenlind@b3.se>
2019-11-14 22:40:58 +0100

    chore: updated dependencies

--- test/mapHelper.test.ts
+++ test/mapHelper.test.ts
@@ -4,7 +4,10 @@ import { Server } from "./testData";
 describe("mapHelper", () => {
   describe("toObject", () => {
     it("can convert map to object", () => {
-      const map = new Map<string, string>([["name", "server1"], ["flag", "SE"]]);
+      const map = new Map<string, string>([
+        ["name", "server1"],
+        ["flag", "SE"]
+      ]);
 
       const server: Partial<Server> = toObject(map);
 
